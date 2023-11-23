b7ffd5d2f0c4a0009407b6efe6013d715a62fd3e
przemyslvw <przemyslvw@gmail.com>
2023-11-23 22:49:06 +0100

    load accounts and buffor

--- run.py
+++ run.py
@@ -1,5 +1,22 @@
+import json
 import argparse
 from InstagramBot import InstagramBot
+
+def load_accounts():
+    try:
+        with open("accounts.json", "r") as file:
+            accounts = json.load(file)
+    except FileNotFoundError:
+        accounts = []
+    return accounts
+
+def load_buffor():
+    try:
+        with open("buffor.json", "r") as file:
+            buffor = json.load(file)
+    except FileNotFoundError:
+        buffor = []
+    return buffor
 
 def main():
 
@@ -10,12 +27,19 @@ def main():
     parser.add_argument("-cm", "--message", help="Comment or message to post")
     parser.add_argument("-del", "--delay", type=int, default=5, help="Delay in seconds between actions")
     args = parser.parse_args()
+    accounts = load_accounts()
+    buffor = load_buffor()
+
+    print(accounts[0])
+    print(buffor)
+
 
     # main functions / loading to buffor JSON
-    InstagramBot().scrape_hashtag_posts(args.hashtag)
-    InstagramBot().scrape_explore_posts(args.hashtag)
+    # bot = create_bot(accounts[0])
+    # bot.scrape_hashtag_posts(args.hashtag)
+    # bot.scrape_explore_posts(args.hashtag)
     # like and comment
-    InstagramBot().load_comments()
+    # InstagramBot().load_comments()
 
 
 if __name__ == "__main__":
